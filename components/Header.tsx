0386ec7ffbbb5ab62ef5e05d8a52784f77ed0499
Santiago <ssanchezgoni01@gmail.com>
2025-07-05 12:57:25 -0300

    Fix header navigation link from /weekly to /

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
           {/* Navigation */}
           <nav className="hidden md:flex items-center space-x-8">
-            <Link href="/weekly" className="text-gray-700 hover:text-gray-900 font-medium">
+            <Link href="/" className="text-gray-700 hover:text-gray-900 font-medium">
               Launching Now
             </Link>
             <span className="text-gray-400 font-medium cursor-not-allowed">
